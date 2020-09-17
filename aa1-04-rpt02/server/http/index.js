f9729794da323876e0b3319204cd6bd0d5a09b70
yeo5578 <62208986+yeo5578@users.noreply.github.com>
2020-09-17 17:01:17 +0900

    Add files via upload

---
+++ aa1-04-rpt02/server/http/index.js
@@ -0,0 +1,19 @@
+// http server : index.js
+
+var http = require('http');
+port = 3000;
+
+var server = http.createServer(function (request, response) {
+  response.writeHeader(200, {
+    "Content-Type": "text/plain"
+  });
+  response.write("Hello HTTP server from node.js"); // WEB response
+  response.write("\n");
+  response.write("Hi, AA03 \n");
+  response.end();
+
+});
+
+server.listen(port);
+console.log("Server Running on " + port + 
+	".\nLaunch http://localhost:" + port);
